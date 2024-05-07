81f4cf8fbe7784ffe7fbbbce0cd34680c7c73353
Rodolfo Fanti <fanti.rodolfo@gmail.com>
2024-05-07 19:36:49 +0200

    added array.fill

---
+++ array_fill.js
@@ -0,0 +1,12 @@
+// A cool builtin Javascript method to initialize an array is Array.fill():
+// the fill() method fills all the elements of an array from a start index to an end index with a static value.
+// the static value can be: a string, a number, a boolean, an object, an array, etc.
+
+// For example:
+const arr = new Array(5).fill(0); // [0, 0, 0, 0, 0]
+
+// Another example:
+const arr = new Array(3).fill("hello"); // ['hello', 'hello', 'hello']
+
+// Another example:
+const arr = new Array(4).fill([1, 2, 3]); // [[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]
